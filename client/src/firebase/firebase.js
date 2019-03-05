bf8923f77ca2559697219716c86549b04763981e
JustinSHong <jhong2009@gmail.com>
2019-03-04 22:28:15 -0500

    set up firebase

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyBuuBQAK5vymdGa_NhkpXBhF3puxeRXi8Q",
+    authDomain: "lambdanotes2.firebaseapp.com",
+    databaseURL: "https://lambdanotes2.firebaseio.com",
+    projectId: "lambdanotes2",
+    storageBucket: "lambdanotes2.appspot.com",
+    messagingSenderId: "848304447874"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
+}
+
+export default Firebase;
