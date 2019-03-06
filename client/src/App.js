ac48e33bed5734319a03f7e98ac784149ab60073
JustinSHong <jhong2009@gmail.com>
2019-03-05 20:37:55 -0500

    set up application theme

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { FirebaseContext } from "./components/Firebase";
 import Navigation from "./components/Navigation";
+import * as ROUTES from "./utils/routes";
 
 const SomeComponent = () => (
     <FirebaseContext.Consumer>
@@ -18,20 +19,6 @@ class App extends Component {
         return (
             <Router>
                 <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
-                    </header>
                     <SomeComponent />
                     <Navigation />
                     <hr />
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,30 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Firebase, { FirebaseContext } from "./components/Firebase";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: "#757ce8",
+            main: "#3f50b5",
+            dark: "#002884",
+            contrastText: "#fff"
+        },
+        secondary: {
+            light: "#ff7961",
+            main: "#f44336",
+            dark: "#ba000d",
+            contrastText: "#000"
+        }
+    }
+});
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
     </FirebaseContext.Provider>,
     document.getElementById("root")
 );
